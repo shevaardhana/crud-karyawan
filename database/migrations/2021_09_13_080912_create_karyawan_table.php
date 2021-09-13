814d30a4be9791ef761e90988d5b915f152ab725
shevaardhana <shevaardhana26@gmail.com>
2021-09-13 15:23:11 +0700

    tambah controller,model,table,request karyawan

---
+++ database/migrations/2021_09_13_080912_create_karyawan_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKaryawanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('karyawan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('mulai_kerja');
+            $table->string('posisi');
+            $table->string('nama');
+            $table->string('rekening');
+            $table->string('tempat_lahir');
+            $table->date('tgl_lahir');
+            $table->longText('alamat');
+            $table->string('no_ktp');
+            $table->string('no_hp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('karyawan');
+    }
+}
