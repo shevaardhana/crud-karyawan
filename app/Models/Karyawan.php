c68fb964f3d0de1ce769f661f4fb7cc9fdfe334e
shevaardhana <shevaardhana26@gmail.com>
2021-09-13 15:27:00 +0700

    Karyawan model

--- app/Models/Karyawan.php
+++ app/Models/Karyawan.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Karyawan extends Model
 {
     use HasFactory;
+
+    protected $table = 'karyawan';
+
+    protected $fillable = [
+        'mulai_kerja','posisi','nama','rekening','tempat_lahir','tgl_lahir','alamat',
+        'no_ktp', 'no_hp'
+    ];
 }
